98ff8260111cf2e60a9aacd686a2dd3c99926181
tienminhvu2507-creator <tienminhvu2507@gmail.com>
2026-04-02 13:46:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/T-o-nh-t-k-ch-b-n-v-nh-nh-n-v-t-s-n-ph-m/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
